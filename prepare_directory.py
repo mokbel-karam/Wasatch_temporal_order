a3959eeefeb1948448470f0b153becff82160f11
mk-95 <karammokbel@gmail.com>
2019-09-06 16:15:41 -0600

    added a singleton class that take care of the directories structures in the working directory

---
+++ prepare_directory.py
@@ -0,0 +1,94 @@
+import subprocess as sp
+import os
+
+class DirStruct:
+    count = 0
+    __instance=None
+    def __init__(self,levels,vars,suthpath,nsteps,extension='ups'):
+        if DirStruct.__instance==None:
+            self.extension= extension
+            self.levels = levels
+            self.nsteps = nsteps
+            self.vars = vars
+            self.suthPath = suthpath
+            self.cwd = self.CWD()
+            self.__generates_names()
+            # for later create a class for structure that allows addition of folders, files ...
+            #self.struct={self.CWD().split('/')[-1]:{'absPath':self.CWD(),'relPath':'.','dirs':{},'files':{}}}
+            DirStruct.count+=1
+            DirStruct.__instance=self
+        else:
+            return DirStruct.getInstance()
+
+    @staticmethod
+    def getInstance():
+        if DirStruct.__instance!=None:
+            return DirStruct.__instance
+        else:
+            raise Exception("This class is not instantiated!")
+
+    def CWD(self):
+        cwdProcess = sp.run(['pwd'], stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+        return cwdProcess.stdout
+
+    def command (self,fileName):
+        return  "python3 compute-temporal-order.py -ups {} -levels {} -nsteps {} -vars {} -suspath {}".format(fileName, self.levels, self.nsteps, self.vars, self.suthPath)
+
+    def __generates_names(self):
+        try:
+            findFileNamesProcess = sp.run(['find *.'+self.extension], stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True, shell=True)
+            self.fileNames = findFileNamesProcess.stdout.split('\n')[:-1]
+            self.folders = [filename.split('.')[0] for filename in self.fileNames]
+            self.filesDict ={}
+        except:
+            print('An error occured while generating the files and folders names.')
+            print('make sure that this type "{}" of files exists in the current working directory!')
+            return False
+
+    def prepare_directories(self):
+        for folder,file in zip(self.folders,self.fileNames):
+            self.filesDict[folder]=file
+        createFoldersCommand = ['mkdir']
+        for folder in self.folders:
+            createFoldersCommand.append(folder)
+        try:
+            mkdir = sp.run(createFoldersCommand, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+            print(self.filesDict)
+            for folder in self.filesDict.keys():
+                os.system('cp $PWD/compute-temporal-order.py $PWD/{}'.format(folder))
+                os.system('cp $PWD/{} $PWD/{}'.format(self.filesDict[folder],folder))
+
+            for folder in self.filesDict.keys():
+                os.system('rm $PWD/{}/run.sh'.format(folder))
+                os.system('echo #!/usr/bin/env bash > $PWD/{}/run.sh'.format(folder))
+                # os.system('echo {} >> $PWD/{}/run.sh'.format(self.command(self.filesDict[folder]),folder))
+                os.system('echo "echo Hello" "{}" >> $PWD/{}/run.sh'.format(folder,folder))
+                os.system('chmod +x $PWD/{}/run.sh'.format(folder))
+        except:
+            print("An error occured while generatiing directories and prepating the scripts to run")
+            return False
+
+        return True
+        # file.write(command(filesDict[folder],levels,nsteps,vars,suthPath))
+# cwd = os.system('pwd')
+#
+# filenames=['taylor-green-vortex-2d-xy-proj-all.ups','taylor-green-vortex-2d-xy-proj-2-3.ups','taylor-green-vortex-2d-xy-proj-1-3.ups','taylor-green-vortex-2d-xy-proj-3.ups']
+#
+# cwd_process = run(['pwd'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+# cwd = cwd_process.stdout
+#
+# for name in filenames:
+#     sp.run(['mkdir',name.split('.')[0])
+#
+# def command (fileName,levels=6,nsteps=10,vars='x-mom,y-mom'):
+#     return  "python2.7 compute-temporal-order.py -ups {} -levels {} -nsteps {} -vars {} -suspath $PWD/../../opt/StandAlone/".format(fileName, levels, nsteps, vars)
+#
+# def worker(Command):
+#     sp.call([Command], shell=True)
+#     return
+#
+# jobs = []
+# for num, name in enumerate(filenames):
+#     p = multiprocessing.Process(target=worker, args=(command(name),))
+#     jobs.append(p)
+#     p.start()
