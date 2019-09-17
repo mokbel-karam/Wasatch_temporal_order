ba22e619a6ae80b83e59df934136fa547bc027cc
mk-95 <karammokbel@gmail.com>
2019-09-17 09:19:49 -0600

    added the feature of parsing the number of iterations

--- exec_time.py
+++ exec_time.py
@@ -1,4 +1,8 @@
 import numpy as np
+import json
+import os
+import pandas as pd
+
 
 class StageCounter:
     def __init__(self):
@@ -22,54 +26,62 @@ class StageCounter:
 
 class Parser:
 
-    def __init__(self, stdoutfile):
-        self.stdoutfile=stdoutfile
-        self.WallTimePerStep = []
-        self.__WallTimePSolve = []
-        self.WallTimePSolveTimestep = []
-        self.WallTimePSolveStage = []
-        self.timesteps=0
-        self.__stgCounter = StageCounter()
-
-    def parse(self, startStep=0,endStep=None):
-        with open(self.stdoutfile) as fp:
+    def __init__(self ):
+        self.totalTime=[]
+        self.totalPsolveTime=[]
+        self.totalPsolve =[]
+        self.totalIterations = []
+    def parse(self, stdoutfile, startStep=0,endStep=None):
+        stdoutfile=stdoutfile
+        WallTimePerStep = []
+        WallTimePSolve = []
+        WallTimePSolveTimestep = []
+        WallTimePSolveStage = []
+        iterationsTimestep = []
+        iterations = []
+        timesteps=0
+        stgCounter = StageCounter()
+        with open(stdoutfile) as fp:
             line = fp.readline()
             count = 1
             while line:
                 currentLine = line.strip()
+                bad=False
 
-                self.__parse_timestep(currentLine,startStep,endStep)
-                self.__parse_pressure(currentLine,startStep,endStep)
+                if self.__exists('Timestep',currentLine):
+                    firstSplit= currentLine.strip().split(' ')
+                    time = [i for i in firstSplit if i]
+                    print(time)
+                    try:
+                        timeVAl = float(time[7].split('=')[1])
+                    except:
+                        bad = True
+                    if ((timesteps>startStep) and (endStep==None or timesteps<=endStep))or not bad:
+                        WallTimePerStep.append(timeVAl)
+                        stgCounter.reinitialize()
+                        WallTimePSolveStage.append(WallTimePSolve)
+                        WallTimePSolveTimestep.append(sum(WallTimePSolve))
+                        WallTimePSolve=[]
+                        iterations.append(sum(iterationsTimestep))
+                        iterationsTimestep=[]
+
+                    timesteps+=1
+
+                if self.__exists('Solve ',currentLine):
+                    if (timesteps>startStep) and (endStep==None or timesteps<=endStep):
+                        firstSplit= currentLine.split(" ")
+                        psolveTime = float(firstSplit[8])
+                        iteration = float(firstSplit[15])
+                        iterationsTimestep.append(iteration)
+                        stgCounter.increment()
+                        WallTimePSolve.append(psolveTime)
 
                 line = fp.readline()
                 count+=1
-
-
-    def __parse_timestep(self,currentLine, startStep,endStep):
-        if self.__exists('Timestep',currentLine):
-            firstSplit= currentLine.strip().split(' ')
-            time = [i for i in firstSplit if i]
-            timeVAl = float(time[6].split('=')[1])
-            if (self.timesteps>startStep) and (endStep==None or self.timesteps<=endStep):
-                self.WallTimePerStep.append(timeVAl)
-                self.__stgCounter.reinitialize()
-                self.WallTimePSolveStage.append(self.__WallTimePSolve)
-                self.WallTimePSolveTimestep.append(self.__psolve_timestep())
-                self.__WallTimePSolve=[]
-
-            self.timesteps+=1
-
-    def __parse_pressure(self,currentLine, startStep,endStep):
-        if self.__exists('Solve ',currentLine):
-            if (self.timesteps>startStep) and (endStep==None or self.timesteps<=endStep):
-                firstSplit= currentLine.split(" ")
-                psolveTime = float(firstSplit[8])
-                self.__stgCounter.increment()
-                self.__WallTimePSolve.append(psolveTime)
-
-    def __psolve_timestep(self):
-        return sum(self.__WallTimePSolve)
-
+            self.totalTime.append(sum(WallTimePerStep))
+            self.totalPsolveTime.append(sum(WallTimePSolveTimestep))
+            self.totalPsolve.append(stgCounter.totalCount)
+            self.totalIterations.append(sum(iterations))
 
     def __exists(self,word,string):
         return True if string.find(word) >=0 else False
@@ -81,13 +93,16 @@ class Parser:
         return False
 
     def total_time(self):
-        return sum(self.WallTimePerStep)
+        return self.totalTime
 
     def total_psolve_time(self):
-        return sum(self.WallTimePSolveTimestep)
+        return self.totalPsolveTime
 
     def total_num_psolve(self):
-        return self.__stgCounter.totalCount
+        return self.totalPsolve
+
+    def total_num_iterations(self):
+        return self.totalIterations
 
     def portion_of_psolve_per_timestep(self):
         return np.average(np.array(self.WallTimePSolveTimestep)/np.array(self.WallTimePerStep))
@@ -100,7 +115,7 @@ class SummaryParser:
         self.data={}
     def parse(self):
         caseData={}
-        with open(self.stdoutfile) as fp:
+        with open(self.stdoutfile, 'rb') as fp:
             line = fp.readline()
             count = 1
             while line:
@@ -132,14 +147,33 @@ class SummaryParser:
 
     def __exists(self,word,string):
         return True if string.find(word) >=0 else False
-# #Exmaple:
-# #=========
-# fileName='log.out'
-#
-# myparser=Parser(fileName)
-# myparser.parse(5)
-#
-# print(myparser.total_time())
-# print(myparser.total_psolve_time())
-# print(myparser.total_num_psolve())
-# print(myparser.portion_of_psolve_per_timestep())
+
+
+myparser = Parser()
+
+d = '.'
+directories=[os.path.join(d, o) for o in os.listdir(d)
+ if os.path.isdir(os.path.join(d,o))]
+
+
+path = '.'
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.log' in file:
+            files.append(os.path.join(r, file))
+
+print(files)
+
+Res = []
+for path in files:
+    Res.append(float(path.split('-')[2][2:]))
+    myparser.parse(path)
+
+dict= {'totalTime':myparser.totalTime,'totalPsolveTime':myparser.totalPsolveTime,'totalPsolve':myparser.totalPsolve,'Res':Res}
+dict = pd.DataFrame(dict).sort_values('Res').to_dict('lists')
+
+with open('./execTimesRK2Proj1.json','w') as file:
+    json.dump(dict,file,indent=4)
