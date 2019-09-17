98b24884a6687705a094dc3a16656e589b748fb3
mk-95 <karammokbel@gmail.com>
2019-09-17 09:34:42 -0600

    added everything into one file in order to execute it on aurora

--- generate_execTimes/run_execTime.py
+++ generate_execTimes/run_execTime.py
@@ -1,3 +1,152 @@
+# -*- coding: utf-8 -*-
+import os
+from xml.dom import minidom
+from shutil import copyfile
+import numpy as np
+
+class Stability:
+
+    def __init__(self,path):
+        self.path = path
+
+        with open(path) as file:
+            data = np.genfromtxt(file,delimiter=',')
+            self.__Reh = list(data[:, 0])
+            self.__CFL = list(data[:, 1])
+            self.__error=list(data[:, 2])
+
+    def get_Reh(self):
+        return self.__Reh
+
+    def get_CFL(self):
+        return self.__CFL
+
+    def get_error(self):
+        return self.__error
+
+
+
+class UPSLuncher:
+    def __init__(self,upsFile,tend,timeInteg,suthPath):
+        self.upsFile = upsFile
+        self.tend = tend
+        self.timeInteg = timeInteg
+        self.suspath=suthPath
+
+        self.xmldoc = minidom.parse(self.upsFile)
+        self.__process_suthpath()
+        self.__set_number_of_patches()
+        self.__find_resolution()
+
+    def __process_suthpath(self):
+        self.suspath = os.path.normpath(self.suspath)
+        self.suspath = os.path.abspath(self.suspath)
+        print (self.suspath)
+
+    def __create_sus_link(self,folder):
+        os.system('ln -fs ' + self.suspath + '/sus '+folder+'/sus')
+        os.system('ln -fs ' + self.suspath + '/tools/extractors/lineextract '+ folder+'/lineextract')
+
+
+# find total number of procs and resolution
+    def __set_number_of_patches(self):
+
+        for node in self.xmldoc.getElementsByTagName('patches'):
+            P = (str(node.firstChild.data).strip()).split(',')
+            P0=int(P[0].split('[')[1])
+            P1=int(P[1])
+            P2=int(P[2].split(']')[0])
+            # node.firstChild.replaceWholeText('[1,1,1]')
+        total_proc = P0*P1*P2
+
+    def __find_resolution(self):
+        for node in self.xmldoc.getElementsByTagName('resolution'):
+            P = (str(node.firstChild.data).strip()).split(',')
+            self.Nx=int(P[0].split('[')[1])
+            self.Ny=int(P[1])
+            self.Nz=int(P[2].split(']')[0])
+
+    def __create_fname(self,CFL,Re):
+        fname = os.path.splitext(self.upsFile)[0] + '-CFL' + str(CFL) +'-Re' + str(Re) + '-tend'+str(self.tend)
+        return fname
+    def __create_logfile_name(self,fname):
+        return fname +'.log'
+
+    def __create_uda_name(self,fname):
+        return fname +'.uda'
+    def __create_newfile_name(self,fname):
+         return './' +fname +'/'+ fname +'.ups'
+
+    def __create_new_files(self,fname):
+        newfile = self.__create_newfile_name(fname)
+        copyfile(self.upsFile, newfile)
+        return newfile
+
+    def __creat_file_to_run(self,fname):
+        return fname +'.ups'
+
+
+
+    def __update_xml(self,fname,dt,mu):
+        file = self.__create_newfile_name(fname)
+        print ('now updating xml for ', file)
+        basename = fname
+        xmldoc = minidom.parse(file)
+        for node in xmldoc.getElementsByTagName('TimeIntegrator'):
+            node.firstChild.replaceWholeText(self.timeInteg)
+
+        for node in xmldoc.getElementsByTagName('filebase'):
+            node.firstChild.replaceWholeText(basename + '.uda')
+
+        for node in xmldoc.getElementsByTagName('delt_min'):
+            dtmin = dt
+            node.firstChild.replaceWholeText(dtmin)
+
+        for node in xmldoc.getElementsByTagName('delt_max'):
+            node.firstChild.replaceWholeText(dt)
+
+        for node in xmldoc.getElementsByTagName('maxTime'):
+            node.firstChild.replaceWholeText(self.tend)
+
+        for node in xmldoc.getElementsByTagName('BasicExpression'):
+            val = node.getElementsByTagName('Constant')[0]
+            val.firstChild.replaceWholeText(mu)
+
+        f = open(file, 'w')
+        xmldoc.writexml(f)
+        f.close()
+
+
+    def __run_fname(self,fname):
+        file = self.__creat_file_to_run(fname)
+        udaName = self.__create_uda_name(fname)
+        outputName =  self.__create_logfile_name(fname)
+        os.environ["SCI_DEBUG"]="ExecOut:+"
+        os_return = os.system('cd '+ fname +';'+'./sus' + ' ' + file + ' > '+outputName)
+        # os.system('rm ' + newfile)
+        os.system('cd '+fname+';'+'rm -f -r *uda* *dot')
+        os.system('cd ..')
+        return True
+
+    def __single_run(self, CFL, Re,U=2):
+        dx = 1.0/self.Nx
+        mu = str(U*dx/Re)
+        dt = str(CFL*dx/U)
+        fname = self.__create_fname(CFL,Re)
+        os.system('mkdir '+fname)
+        self.__create_sus_link(fname)
+        newfile = self.__create_new_files(fname)
+        self.__update_xml(fname,dt,mu)
+        run = self.__run_fname(fname)
+
+    def run(self,CFL,Re,U=2):
+        if type(CFL) == list and type(Re)==list:
+            for cfl, re in zip(CFL,Re):
+                self.__single_run(cfl,re,U)
+
+        else:
+            self.__single_run(CFL,Re,U)
+
 
 import argparse
 from generate_execTimes.RunningAtStability import *
